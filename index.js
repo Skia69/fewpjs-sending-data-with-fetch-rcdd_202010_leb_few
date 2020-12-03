fce576e87970454702696a6cbab4c2da8ce99397
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-03 12:16:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,27 +20,3 @@ return fetch('http://localhost:3000/users', options)
   .then(object => document.body.innerHTML = object["id"])
   .catch(err => document.body.innerHTML= err.message)
 }
-
-
-// function submitData( name, email ) {
-//   return fetch( 'http://localhost:3000/users', {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Accept": "application/json"
-//       },
-//       body: JSON.stringify( {
-//         name,
-//         email
-//       } )
-//     } )
-//     .then( function ( response ) {
-//       return response.json()
-//     } )
-//     .then( function ( object ) {
-//       document.body.innerHTML = object[ "id" ]
-//     } )
-//     .catch( function ( error ) {
-//       document.body.innerHTML = error.message
-//     } )
-// }
