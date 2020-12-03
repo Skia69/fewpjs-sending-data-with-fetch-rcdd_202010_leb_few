bc0f5c9116eb5cb532c8795c9583266ac1e42f91
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-03 11:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,13 +36,8 @@ function submitData( name, email ) {
         email
       } )
     } )
-    .then( function ( response ) {
-      return response.json()
-    } )
-    .then( function ( object ) {
-      document.body.innerHTML = object[ "id" ]
-    } )
-    .catch( function ( error ) {
-      document.body.innerHTML = error.message
-    } )
+  return fetch("http://localhost:3000/users", dataObject)
+  .then(response => response.json)
+  .then(object =>  document.body.innerHTML = object["id"])
+  .catch(error => document.body.innerHTML= error.message)
 }
