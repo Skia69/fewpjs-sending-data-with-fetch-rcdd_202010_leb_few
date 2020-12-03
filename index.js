5ad9d288ab87d04c22e121c8096c9d907456fbb8
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-03 11:52:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+   let p= {
+      name,
+      email
+    }
+  
+
+let dataObject = {
+  method: "POST",
+  credentials: 'same-origin',
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }, 
+  body: JSON.stringify(p)
+  
+};
+
+return fetch("http://localhost:3000/users", dataObject)
+  .then(response => response.json)
+  .then(object =>  document.body.innerHTML = object["id"])
+  .catch(error => document.body.innerHTML= error.message)
+}
